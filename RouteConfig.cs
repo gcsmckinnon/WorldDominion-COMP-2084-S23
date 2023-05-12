e68baae5d82ebeffb4d3a1062b564398f6049dee
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-05-12 18:39:55 -0400

    Abstracted the routes for easier maintainability.

---
+++ RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿namespace WorldDominion
+{
+    public class RouteConfig
+    {
+        public static void ConfigureRoutes(IEndpointRouteBuilder routes)
+        {
+            routes.MapControllerRoute(
+                name: "about",
+                pattern: "about",
+                defaults: new { controller = "Home", action = "About" });
+
+            routes.MapControllerRoute(
+                name: "contact",
+                pattern: "contact",
+                defaults: new { controller = "Home", action = "Contact" });
+
+            routes.MapControllerRoute(
+                name: "privacy",
+                pattern: "privacy",
+                defaults: new { controller = "Home", action = "Privacy" });
+
+            routes.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+}
