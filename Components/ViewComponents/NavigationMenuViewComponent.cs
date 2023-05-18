82fa6015ad9465e962ccfaf593fe9ab269322308
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-05-18 09:09:57 -0400

    Fixed the architecture to reflect best practices on the navigation.

---
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WorldDominion.Models;
+
+namespace WorldDominion.Components.ViewComponents
+{
+    [ViewComponent(Name = "NavigationMenu")]
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var menuItems = new List<MenuItem>
+        {
+            new MenuItem { Controller = "Home", Action = "Index", Label = "Home" },
+            new MenuItem { Controller = "Categories", Action = "Index", Label = "Categories" },
+            new MenuItem { Controller = "Home", Action = "About", Label = "About" },
+            new MenuItem { Controller = "Home", Action = "Contact", Label = "Contact" },
+            new MenuItem { Controller = "Home", Action = "Privacy", Label = "Privacy" },
+        };
+
+            return View(menuItems);
+        }
+    }
+}
