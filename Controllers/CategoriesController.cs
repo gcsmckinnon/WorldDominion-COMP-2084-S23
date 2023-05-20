30612db4b668b4ffc89e02a4179a7edd8cfa3f73
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-05-20 11:13:59 -0400

    Added the mocked create method and validation

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -5,16 +5,19 @@ namespace WorldDominion.Controllers
 {
     public class CategoriesController : Controller
     {
-        public List<Category> Categories { get; set; }
+        public static List<Category> Categories { get; set; }
 
         public CategoriesController()
         {
-            Categories = new List<Category>()
+            if (Categories == null)
             {
-                new Category { Id = 1, Name = "Dairy and Eggs", Description = "Find all the best milks and ovums" },
-                new Category { Id = 2, Name = "Fruits and Vegetables", Description = "Lettuce turn up the beet! It's kind of a big dill." },
-                new Category { Id = 3, Name = "Meats", Description = "We're a purveyor of fine meats." },
-            };
+                Categories = new List<Category>()
+                {
+                    new Category { Id = 1, Name = "Dairy and Eggs", Description = "Find all the best milks and ovums" },
+                    new Category { Id = 2, Name = "Fruits and Vegetables", Description = "Lettuce turn up the beet! It's kind of a big dill." },
+                    new Category { Id = 3, Name = "Meats", Description = "We're a purveyor of fine meats." },
+                };
+            }
         }
 
         public IActionResult Index()
@@ -25,6 +28,25 @@ namespace WorldDominion.Controllers
         public IActionResult Browse(int Id)
         {
             var category = Categories.Find(category => category.Id == Id);
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                Categories.Add(category);
+                
+                return RedirectToAction("Index");
+            }
+
             return View(category);
         }
     }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,9 +1,21 @@
-﻿namespace WorldDominion.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorldDominion.Models
 {
     public class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        // https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/validation?view=aspnetcore-7.0
+
+        [Range(1, 999999, ErrorMessage = "Must be between 1 and 999999")]
+        [Display(Name = "Category ID")] // Sets an alias
+        public int Id { get; set; } = -1;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "You must provide a Category Name")]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; } = String.Empty;
+
+        // If we want an option to NOT REQUIRED we need to use the ? decorator to indicate the field is nullable
+        [Display(Name = "Category Description")]
+        public string? Description { get; set; } = String.Empty;
     }
 }
