1f8a34faf651dd2d26c241e05d83ff575749e4e1
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-07-16 10:22:26 -0400

    Added public view for shops

---
+++ Controllers/ShopController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WorldDominion.Data;
+
+namespace WorldDominion.Controllers
+{
+    public class ShopController : Controller
+    {
+        // Property for our database connection
+        private ApplicationDbContext _context;
+
+        // Constructor
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var departments = await _context.Departments
+                .OrderBy(department => department.Name)
+                .ToListAsync();
+
+            return View(departments);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var departmentWithProducts = await _context.Departments
+                .Include(department => department.Products)
+                .FirstOrDefaultAsync(department => department.Id == id);
+
+            return View(departmentWithProducts);
+        }
+    }
+}
