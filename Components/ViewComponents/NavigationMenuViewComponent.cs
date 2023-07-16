ccdd482adbb1581165111833c9066ba67d81f001
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-07-16 10:21:29 -0400

    Modified to support authentication and authorization

--- Components/ViewComponents/NavigationMenuViewComponent.cs
+++ Components/ViewComponents/NavigationMenuViewComponent.cs
@@ -10,14 +10,15 @@ namespace WorldDominion.Components.ViewComponents
             var menuItems = new List<MenuItem>
         {
             new MenuItem { Controller = "Home", Action = "Index", Label = "Home" },
+            new MenuItem { Controller = "Shop", Action = "Index", Label = "Shop" },
             new MenuItem { Controller = "Departments", Action = "Index", Label = "Departments", DropdownItems = new List<MenuItem> {
                 new MenuItem { Controller = "Departments", Action = "Index", Label = "List" },
                 new MenuItem { Controller = "Departments", Action = "Create", Label = "Create" },
-            } },
+            }, Authorized = true, AllowedRoles = new List<string> { "Administrator" } },
             new MenuItem { Controller = "Products", Action = "Index", Label = "Products", DropdownItems = new List<MenuItem> {
                 new MenuItem { Controller = "Products", Action = "Index", Label = "List" },
                 new MenuItem { Controller = "Products", Action = "Create", Label = "Create" },
-            } },
+            }, Authorized = true, AllowedRoles = new List<string> { "Administrator" } },
             new MenuItem { Controller = "Home", Action = "About", Label = "About" },
             new MenuItem { Controller = "Home", Action = "Contact", Label = "Contact" },
             new MenuItem { Controller = "Home", Action = "Privacy", Label = "Privacy" },
