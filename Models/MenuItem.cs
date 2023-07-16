316715e775fe4947e403982d711a4dc9d66fcba1
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-07-16 10:21:11 -0400

    Modified to support authentication and authorization

--- Models/MenuItem.cs
+++ Models/MenuItem.cs
@@ -6,5 +6,7 @@
         public string Action { get; set; }
         public string Label { get; set; }
         public List<MenuItem> DropdownItems { get; set; }
+        public bool? Authorized { get; set; }
+        public List<string>? AllowedRoles { get; set; }
     }
 }
