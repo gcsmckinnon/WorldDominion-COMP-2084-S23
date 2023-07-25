ce80f9e3367bbbf60147670fe0604ed07ff97eab
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-07-25 14:44:14 -0400

    Add to cart feature added to lesson

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using WorldDominion.Data;
+using WorldDominion.Models;
 
 namespace WorldDominion.Controllers
 {
@@ -39,6 +42,57 @@ namespace WorldDominion.Controllers
                 .FirstOrDefaultAsync(product => product.Id == id);
 
             return View(product);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddToCart(int productId, int quantity)
+        {
+            // Get our logged in user
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            // Attempt to get a cart
+            var cart = await _context.Carts
+                .FirstOrDefaultAsync(cart => cart.UserId == userId && cart.Active == true);
+
+            // Check that we have don't have an active cart
+            if (cart == null)
+            {
+                cart = new Models.Cart { UserId = userId };
+                await _context.AddAsync(cart);
+                await _context.SaveChangesAsync();
+            }
+
+            // Find our product
+            var product = await _context.Products
+                .FirstOrDefaultAsync(product => product.Id == productId);
+
+            // GTFO if no product found
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Create a new cart item
+            var cartItem = new CartItem
+            {
+                Cart = cart,
+                Product = product,
+                Quantity = quantity,
+                Price = (decimal)product.MSRP,
+            };
+
+            // If valid, do all the goodness
+            if (ModelState.IsValid)
+            {
+                await _context.AddAsync(cartItem);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("ProductDetails", new { id = productId });
+            }
+
+            // Otherwise, GTFO
+            return NotFound();
         }
     }
 }
